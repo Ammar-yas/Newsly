248e7806c60b93a1de34a87fc35f9046fcfedd64
Ammar Yasser <plus.ammaryasser@gmail.com>
2019-07-15 10:17:51 +0200

    Set top headlines as main activity fragment

--- app/src/main/java/com/intdv/newsly/activities/MainActivity.java
+++ app/src/main/java/com/intdv/newsly/activities/MainActivity.java
@@ -3,8 +3,12 @@ package com.intdv.newsly.activities;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.intdv.newsly.R;
+import com.intdv.newsly.fragments.TopHeadlinesFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showTopHeadlines();
+    }
+
+    private void showTopHeadlines() {
+        TopHeadlinesFragment topHeadlinesFragment = new TopHeadlinesFragment();
+        replaceMainFrameLayout(topHeadlinesFragment);
+    }
+
+    private void replaceMainFrameLayout(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.mainActivityFrameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
