9f14692bc357da8c7ab04673ab9206393192b8f7
Ammar Yasser <plus.ammaryasser@gmail.com>
2019-07-21 17:26:58 +0200

    implement splash screen activity and add lottie animation to it

---
+++ app/src/main/java/com/intdv/newsly/activities/SplashScreenActivity.java
@@ -0,0 +1,47 @@
+package com.intdv.newsly.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.AlphaAnimation;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.intdv.newsly.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private final static int SPLASH_DISPLAY_LENGTH = 2000;
+    @BindView(R.id.splashAppNameTV)
+    TextView appNameTv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        ButterKnife.bind(this);
+        startAppNameAnimation();
+        startSplashTimer();
+    }
+
+    private void startAppNameAnimation() {
+        AlphaAnimation fadeIn = new AlphaAnimation(0.0f, 1.0f);
+        fadeIn.setDuration(1500);
+        appNameTv.startAnimation(fadeIn);
+    }
+
+    private void startSplashTimer() {
+        new Handler().postDelayed(this::startNextActivity, SPLASH_DISPLAY_LENGTH);
+    }
+
+    private void startNextActivity() {
+        Intent mainActivityIntent = new Intent(this, MainActivity.class);
+        startActivity(mainActivityIntent);
+        finish();
+    }
+
+}
